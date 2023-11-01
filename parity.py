19b50fda8f95e807f4abdde958c14e999732b84b
AlessioLucciola <alessiolucciola98@hotmail.it>
2023-11-01 14:49:41 +0100

    Changed parity functions

--- parity.py
+++ parity.py
@@ -1,13 +1,16 @@
 import graph_creation as gc
 import networkx as nx
 
-def parity_game(n_nodes=10):
+def parity_game(n_nodes=15):
     G = gc.Graph(n_nodes) # Create graph
     strategy_si = solve_parity_strategy_improvement(G)
     print("Winning regions strategy improvement: " + str(strategy_si))
     strategy_zi = solve_parity_zielonka(G)
     print("Winning strategy Zielonka: " + str(strategy_zi))
-    G.visualize_graph()
+    #G.visualize_graph()
+
+    diff = find_differences(strategy_si, strategy_zi)
+    print("Differences among the strategies: " + str(diff))
 
 def solve_parity_strategy_improvement(G):
     parities = {node: node for node in G.graph.nodes} # Assign a priority to each node
@@ -21,7 +24,7 @@ def solve_parity_strategy_improvement(G):
             successors = list(G.graph.successors(node))
             if successors: #If there are successors
                 successor_parities = [parities[successor] for successor in successors] # Calculate the parity of the successors
-                node_parity = max(successor_parities) # Select the max parity
+                node_parity = max(successor_parities) if player == 0 else min(successor_parities) # Select the max parity
 
                 # If the parity of the node is different from the max successor parity
                 # update the parity of the node
@@ -39,24 +42,24 @@ def solve_parity_strategy_improvement(G):
 
 def solve_parity_zielonka(G):
     Gc = G.graph.copy()
-    priorities = {node: 0 if node in G.player0_nodes else 1 for node in G.graph.nodes} # Assign a priority to each node
+    priorities = {node: node for node in G.graph.nodes} # Assign a priority to each node
     strategy = {node: -1 for node in G.graph.nodes} # Initialize the strategy of each node with -1
-    components = []
+    components = [] # Initialize a list where to put each component of the graph
 
-    while len(Gc.nodes) > 0:
-        highest_priority = max(priorities.values())
-        component = dfs(Gc, highest_priority, [], [])
-        components.append(component)
-        for n in component:
+    while len(Gc.nodes) > 0: # Iterate until all components are found
+        highest_priority = max(priority for priority in priorities.values() if priority in Gc.nodes) # Get the highest priority in the graph
+        component = dfs(Gc, highest_priority, [], []) # Apply dfs from the highest priority node to get the associated component
+        components.append(component) 
+        for n in component: # Remove the nodes in the found component
             Gc.remove_node(n)
 
-        for component in components:
-            subgame = G.graph.subgraph(component)
-            for n in subgame.nodes():
-                if priorities[n] % 2 == 0:
-                    strategy[n] = 0
-                else:
-                    strategy[n] = 1
+    for component in components: # For each component found
+        subgame = G.graph.subgraph(component) # Create a subgame from the initial graph
+        for n in subgame.nodes(): # Assign winning strategies
+            if priorities[n] % 2 == 0:
+                strategy[n] = 0
+            else:
+                strategy[n] = 1
     return strategy
 
 
@@ -68,28 +71,7 @@ def dfs(G, node, component, visited):
             dfs(G, successor, component, visited)
     return component
 
-'''
-def solve_parity_zielonka(G):
-    priority = [0 for _ in range(len(G.graph.nodes))]
-    winner = [0 for _ in range(len(G.graph.nodes))]
-
-    for node in range(len(G.graph.nodes)):
-        priority[node] = max(G.graph.nodes[node])
-
-        print(priority)
-
-    for i in range(max(priority), -1, -1):
-        for node in range(len(G.graph.nodes)):
-            if priority[node] == i:
-                min_priority = float('inf')
-                for neighbor in G.graph.nodes[node]:
-                    if priority[neighbor] < min_priority:
-                        min_priority = priority[neighbor]
-                        winner[node] = winner[neighbor]
-
-                priority[node] = min_priority
-
-    #return winner
-'''
+def find_differences(strategy1, strategy2):
+    return [key for key in strategy1 if strategy1[key] != strategy2[key]]
 
 parity_game()
